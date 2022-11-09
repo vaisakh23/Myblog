90fd588e53f95399ed3852cb102a9c4d599f8bd9
vaisakh <vaisakhsindhu11@gmail.com>
2022-11-09 20:59:36 +0530

    login form added

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,27 @@
+from flask import render_template, flash, redirect, url_for
 from . import app
+from .forms import LoginForm
+
+user = {"username": "vaisakh"}
+posts = [
+    {
+        "author": {"username": "jhon"},
+        "body": "Beautiful day it is!"
+    },
+    {
+        "author": {"username": "mike"},
+        "body": "rainly day"
+    }
+    ]
 
 @app.route("/")
 def index():
-	return "<h1>Hello World</h1>"
+	return render_template('index.html', user=user, posts=posts)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'login as {form.username.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
