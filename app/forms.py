86df685d58001dd317c577ed8883e6f3554da0ce
vaisakh <vaisakhsindhu11@gmail.com>
2022-11-10 21:31:37 +0530

    registration form added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
+    check_password = PasswordField('Check Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from . import app
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 user = {"username": "vaisakh"}
 posts = [
@@ -25,3 +25,11 @@ def login():
         flash(f'login as {form.username.data}')
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Successfully registered as {form.username.data}')
+        return redirect(url_for('login'))
+    return render_template('signup.html', form=form)
